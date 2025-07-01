a69930a9d98b76b727239e56abf538e4a0ec509d
BBLUE-1112 <nilkanta@sourcedeskglobal.com>
2025-07-01 17:29:36 +0530

    Update next.config.mjs
    domain array

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = { 
     images: {
-    domains: ['netfirst.ca','gwebpro.com','localhost:8000','127.0.0.1:8000','127.0.0.1','localhost','167.71.231.165'],
+    domains: ['netfirst.ca','gwebpro.com','localhost:8000','127.0.0.1:8000','127.0.0.1','localhost','167.71.231.165', '167.71.231.165:8000'],
   },
 
 };
